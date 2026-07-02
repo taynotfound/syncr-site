28df230695dd0741ad185b3e7697c7897dc8133a
Pixel <pixel@taymaerz.de>
2026-07-02 11:08:29 +0000

    Gallery: add auto-updater screenshot as 6th carousel slide

--- app.js
+++ app.js
@@ -371,6 +371,7 @@
     'assets/shots/discord-3.png',
     'assets/shots/discord-4.png',
     'assets/shots/discord-5.png',
+    'assets/shots/popup-autoupdate.png',
   ];
   var shotIdx = 0;
   var shotMain = document.getElementById('shotMain');
